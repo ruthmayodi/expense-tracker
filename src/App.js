d809ce4f3c5fff70fc68620525322ff7e33f2818
Ruth Mayodi <ruthmayodi@gmail.com>
2020-08-11 17:38:31 -0400

    added header and balance components also power outage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header/>
+      <div className = "Container">
+        <Balance/>
+      </div>
     </div>
   );
 }
